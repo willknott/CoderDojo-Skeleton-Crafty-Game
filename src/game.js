f6db6ca2aacd9ff71264cb1b5959e6e1785adb98
Will Knott <Will@WillKnott.ie>
2015-07-20 14:14:25 +0100

    Interaction
    Now the player character reacts with the blocks and boundaries
    Also changed the playfield size

--- src/game.js
+++ src/game.js
@@ -1,8 +1,8 @@
 Game = {
   // This defines our grid's size and the size of each of its tiles
 	map_grid: {
-		width:  24,
-		height: 16,
+		width:  50,
+		height: 40,
 		tile: {
 			width:  16,
 			height: 16
